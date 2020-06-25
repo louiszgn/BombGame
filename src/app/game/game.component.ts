5bbb9034dc2fe6e07c1411f7cd9594f8cd31b166
Louis Zeggane <louiszeggane@gmail.com>
2020-06-25 21:32:54 +0200

    Début de gameplay

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -11,6 +11,9 @@ import { Carte } from '../carte';
 export class GameComponent implements OnInit {
   title = 'The game';
   public nbJoueurs: string;
+  public numJoueur:number;
+  public deminageTotal: number;
+  public deminageTrouve: number;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -22,7 +25,6 @@ export class GameComponent implements OnInit {
     this.beginGame();
   }
 
-  
 
   public beginGame(){
     let gamePlayers: Array<string>;
@@ -70,9 +72,10 @@ export class GameComponent implements OnInit {
   let carteDeminage: number = Math.floor(gameCards*0.25);
   let carteLeurre: number = gameCards - (carteDeminage+1);
   var i:number ;
+  this.deminageTotal = carteDeminage;
 
   for( i=carteDeminage ; i> 0 ; i-- ) {
-      let card: Carte = new Carte('Deminage');
+      let card: Carte = new Carte('Déminage');
       this.deck.push(card);
   }
 
@@ -96,15 +99,44 @@ export class GameComponent implements OnInit {
         element.addCarte(this.deck[rand]);
         this.deck.splice(rand, 1);
       }
-      console.log(element.deck);
     });
   }
 
   public game(){
-    let numJoueur: number = 1;
+    this.numJoueur = 1;
+    this.deminageTrouve = 0;
 
     this.joueurs.forEach(element => {
+      let compteur: number = 0;
+
       document.getElementById("game-container").innerHTML += '<div id="'+element.JoueurNumber+'" class="div-joueur col-4 border-1"></div>';
+
+      element.deck.forEach(carte => {
+        // document.getElementById(element.JoueurNumber.toString()).innerHTML += '<div class="div-carte" (click)="selectedCard($event)">'+ carte.nom+'</div>';
+        document.getElementById(element.JoueurNumber.toString()).innerHTML += '<div id="'+element.JoueurNumber.toString()+'-'+compteur+'" class="div-carte" (click)="selectedCard($event)">'+compteur+'</div>';
+        compteur++;
+      });
     });
+
+    document.getElementById(this.numJoueur.toString()).classList.add("active-joueur");
+  }
+
+  public selectedCard(e){
+    let selectedPlayer = e.target.parentNode.id;
+
+    if(selectedPlayer == this.numJoueur) return; // Ne fais rien si la carte séléctionnée appartient au joueur actuel
+
+    let cardInfos = e.target.id.split('-'); // [0] = Id du joueur ||||| [1] = Position de la carte
+    let cardName = this.joueurs[cardInfos[0]].deck[cardInfos[1]].nom;
+    this.joueurs[cardInfos[0]].deck.splice(cardInfos[1], 1); // IL FAUT ACTUALISER LES DIV DES CARTES POUR QU'ELLES DISPARAISSENT APRES CA
+
+    alert("C'était une carte <strong>"+cardName+"</strong> !");
+
+    document.getElementById(this.numJoueur.toString()).classList.remove("active-joueur");
+    this.numJoueur = cardInfos[0];
+    document.getElementById(this.numJoueur.toString()).classList.add("active-joueur");
+
+    if (cardName === "Déminage") this.deminageTrouve++; // Sûrement à mettre dans la fonction qui vérifie si la partie est finie
+
   }
 }
